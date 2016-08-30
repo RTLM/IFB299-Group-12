17f35cdb42c0c03f122ffaeeb82873f68ade29e3
bannid <bannid@gmail.com>
2016-08-30 15:43:24 +1000

    Create leftNavBar.php

---
+++ Banni/leftNavBar.php
@@ -0,0 +1,29 @@
+<div class ="BJleftNavBar">
+        <div class="container">
+          <div class ="BJleftCollapseButton" type="button" data-toggle="collapse" data-target="#staff">Customer</div>
+          <div id="staff" class="collapse BJleftCollapseButtonSubButton">
+              <!--Not a good idea to use inline CSS however it was clashing with some other class..-->
+              <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">New Delivery</div></a>
+              <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">Track Delivery</div></a>
+          </div>
+        </div>
+        <div class="container">
+          <div class ="BJleftCollapseButton" type="button" data-toggle="collapse" data-target="#owner">Staff</div>
+          <div id="owner" class="collapse BJleftCollapseButtonSubButton">
+              <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">Admin</div></a>
+              <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">Driver</div></a>
+          </div>
+        </div>
+        <div class="container">
+          <div class ="BJleftCollapseButton" type="button" data-toggle="collapse" data-target="#aboutLeftBar">About Us</div>
+          <div id="aboutLeftBar" class="collapse BJleftCollapseButtonSubButton">
+              <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">About Us</div></a>
+          </div>
+        </div>
+        <div class="container">
+          <div class ="BJleftCollapseButton" type="button" data-toggle="collapse" data-target="#contact">Contact</div>
+          <div id="contact" class="collapse BJleftCollapseButtonSubButton">
+            <a href=""style="text-decoration: none"><div class ="BJleftNavBarLinks">Contact Us</div></a>
+          </div>
+        </div>
+      </div>
