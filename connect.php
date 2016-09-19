72758f4eb79cc51d7a0557a8227d37f1fdd5f7c6
RTLM <joshrussellahern@gmail.com>
2016-09-20 03:10:51 +1000

    Hosted Database

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database = "ifb299";
+$servername = "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "zb6tlk01gqtov2fy";
+$password = "cv6ok6emcaqd28uc";
+$database = "zqqzsvykt8d5j90x";
 $counter = 0;
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
