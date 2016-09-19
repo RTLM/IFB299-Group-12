7d70cf88513180ad55fefa21ef25935d47041ad3
RTLM <joshrussellahern@gmail.com>
2016-09-20 03:16:57 +1000

    Hosted Database

--- connect.php
+++ connect.php
@@ -1,11 +1,13 @@
 <?php
-$servername = "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "zb6tlk01gqtov2fy";
-$password = "cv6ok6emcaqd28uc";
-$database = "zqqzsvykt8d5j90x";
-$counter = 0;
+$url = getenv(mysql://zb6tlk01gqtov2fy:cv6ok6emcaqd28uc@sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zqqzsvykt8d5j90x);
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	 
