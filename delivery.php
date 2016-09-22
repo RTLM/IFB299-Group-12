827cce05a65d0ef657de1349f46d39fdf6c42496
RTLM <joshrussellahern@gmail.com>
2016-09-23 02:20:44 +1000

    Restoring Changes

--- delivery.php
+++ delivery.php
@@ -10,9 +10,9 @@
             include 'PHPfunctions.php';
             $db = new database;
             $db->connectToDatabase();
-            $sqlSt = "SELECT orderNo, accountNo, destination, receiversName, status, pickUp, orderDate FROM orders WHERE accountNo = ".$_SESSION["accountNo"].";";
+            $sqlSt = "SELECT orderNo, accountNo, destination, status, pickUp, orderDate FROM orders WHERE status = 'Pending';";
             $result = $db->getArrayOfValues($sqlSt);
-            if (true) {
+            if (isset($result)) {
                 foreach($result as $row){?>
                     <div class="horizontal-center">
                         <div class="row">
@@ -22,10 +22,9 @@
                                         <div class="panel-heading">
                                             Order #<?php echo $row['orderNo']; ?>
                                         </div>
-                                        <div class="panel-body">
-                                            Sent To: <?php echo $row['receiversName']; ?><br>
-                                            Destination: <?php echo $row['destination']; ?><br>
-                                            Sent From: <?php echo $row['pickUp']; ?><br>
+                                        <div class="panel-body"> 
+											Pick-Up From: <?php echo $row['pickUp']; ?><br>
+                                            Destination: <?php echo $row['destination']; ?><br>                                            
                                             Status: <?php echo $row['status']; ?><br>
                                             Estimated Delivery: <?php 
                                             $date = new DateTime($row['orderDate']);
@@ -35,7 +34,7 @@
                                         <form action = "markComplete.php" method="post" name="statusMarker">
                                             <div class="btn-container-right">
                                                 <button type="submit" name="statusComplete" value=<?php echo $row['orderNo']; ?> class="btn btn-success btn-space">Mark As Complete</button>
-                                                <a href="#" class="btn btn-info btn-space" name role="button">Order Details</a>					
+                                                <a href="orderDetails.php?order=<?php echo $row['orderNo']; ?>" class="btn btn-info btn-space" name role="button">Order Details</a>					
                                             </div>
                                         </form>
                                     </div>
@@ -49,8 +48,17 @@
                 echo "0 results";
             }
         ?>
-        <?php
-            include "tail.php";
-        ?>
+          <footer>
+                  <p>&copy; 2016 Company, Inc.</p>
+          </footer>
+        </div> <!-- /container -->
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../../dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     </body>
 </html>
