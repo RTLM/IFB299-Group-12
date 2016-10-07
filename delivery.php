c7831c71bc816e3e324d9f2d57d96cfc03c88d0d
RTLM <joshrussellahern@gmail.com>
2016-10-07 10:25:44 +1000

    Formatting of dropdown status button

--- delivery.php
+++ delivery.php
@@ -38,10 +38,21 @@
                                             date_add($date, date_interval_create_from_date_string('5 weekdays'));
                                             echo $date->format('d/m/Y'); ?>
                                         </div>
-                                        <form action = "markComplete.php" method="post" name="statusMarker">
+                                        <form action = "markComplete.php" method="post" name="statusMarker">								
                                             <div class="btn-container-right">
-                                                <button type="submit" name="statusComplete" value=<?php echo $row['orderNo']; ?> class="btn btn-success btn-space">Mark As Complete</button>
-                                                <a href="orderDetails.php?order=<?php echo $row['orderNo']; ?>" class="btn btn-info btn-space" name role="button">Order Details</a>					
+												<div class="dropdown div-inline">
+												<button class="btn btn-primary btn-space dropdown dropdown-toggle" type="button" data-toggle="dropdown">Update Status
+													<span class="caret"></span>
+												</button>
+												<ul class="btn-space dropdown-menu dropdown-menu-right">
+													<li><a href="#">Ready for Pickup</a></li>
+													<li><a href="#">On Way to Warehouse</a></li>
+													<li><a href="#">At Warehouse</a></li>
+													<li><a href="#">With Driver For Delivery</a></li>
+													<li><a href="#">Complete</a></li>
+												</ul>
+												</div>
+                                                <a href="orderDetails.php?order=<?php echo $row['orderNo']; ?>" class="btn btn-info btn-space" name role="button">Order Details</a>							
                                             </div>
                                         </form>
                                     </div>
@@ -60,4 +71,9 @@
             include "tail.php";
         ?>
     </body>
+	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+
 </html>
