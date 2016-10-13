bc9252ce411d3030d901ff3e490a8aba99f97337
yxngstates <jeremyskidmore96@gmail.com>
2016-10-14 09:07:58 +1000

    Add files via upload

--- about.php
+++ about.php
@@ -10,33 +10,15 @@
         <!-- Main jumbotron for a primary marketing message or call to action -->
         <div class="jumbotron">
           <div class="container">
-            <h1>Welcome!</h1>
-            <p>To On The Spot delivery service. Click the button below to begin your order</p>
+            <h1>About Us</h1>
+			<div class="p1">
+            <p>On The Spot Delivery was founded by Bill Wiley and currently employs two delivery drivers. The combined expertise of all employees allows local Brisbane customers to enjoy the premium service standards which On The Spot Delivery is known for. </p>
             <p>
-                <a class="btn btn-primary btn-lg" href="order.php" role="button">Start Order &raquo;</a>
+                
             </p>
           </div>
         </div>
-        <div class="container">
-          <!-- Example row of columns -->
-          <div class="row">
-            <div class="col-md-4">
-              <h2>Heading</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div class="col-md-4">
-              <h2>Heading</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-           </div>
-            <div class="col-md-4">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-            </div>
-          </div>
-		</div>
+        
         <?php
             include "tail.php";
         ?>
