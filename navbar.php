b58de15bfd84108dfce8d8f0fca3063d71cb4cee
bannid <bannid@gmail.com>
2016-10-26 19:43:55 +1000

    Echo bug

--- navbar.php
+++ navbar.php
@@ -1,49 +1,47 @@
 <?php 
-
 session_start();
 include 'PHPfunctions.php';
-
 ?>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
       <div class="navbar-header">
         <a class="navbar-brand" href=<?php 
-		switch ($_SESSION["accountType"]) {
-			case "Customer":
-				echo "index.php";
-				break;
-			case "Driver":
-				echo "assigned.php";
-				break;
-			case "Owner":
-				echo "unassigned.php";
-				break;
-		}
-		?>>On The Spot</a>
+    switch ($_SESSION["accountType"]) {
+      case "Customer":
+        echo "'"."index.php"."'";
+        break;
+      case "Driver":
+        echo "assigned.php";
+        break;
+      case "Owner":
+        echo "unassigned.php";
+        break;
+    }
+    ?>>On The Spot</a>
       </div>
-      <div id="navbar" class="navbar-collapse collapse">	  	  
+      <div id="navbar" class="navbar-collapse collapse">        
             <ul class="nav navbar-nav">
-			<?php 
-				echo updateNav($_SESSION['accountType']);
-			?>
+      <?php 
+        echo updateNav($_SESSION['accountType']);
+      ?>
             </ul>
             <?php if ($_SESSION["login"]){ ?>
-			<form class="navbar-form navbar-right">
+      <form class="navbar-form navbar-right">
                 <div class="dropdown">
                   <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><?php echo $_SESSION["emailId"];?>
                     <span class="caret"></span>
                   </button>
                   <ul class="dropdown-menu">
-					<?php if ($_SESSION["accountType"] == "Customer"){ ?>
-					<li><a href="accountDetails.php">Account Details</a></li>
-					<?php } ?>
+          <?php if ($_SESSION["accountType"] == "Customer"){ ?>
+          <li><a href="accountDetails.php">Account Details</a></li>
+          <?php } ?>
                     <li><a href="logout.php">Log Out</a></li>
                   </ul>
                 </div>
             </form>
-			<?php } else { ?>
-			<form class="navbar-form navbar-right" name="loginForm"action="loginPage.php"method="post">
+      <?php } else { ?>
+      <form class="navbar-form navbar-right" name="loginForm"action="loginPage.php"method="post">
                 <div class="form-group">
                   <input type="text" placeholder="Email" class="form-control"name="email">
                 </div>
@@ -53,7 +51,7 @@ include 'PHPfunctions.php';
                 <button type="submit" class="btn btn-success"action="submit">Sign in</button>
                 <a href="register.php" class="btn btn-primary" role="button">Create Account</a>
             </form>
-			<?php } ?>
+      <?php } ?>
       </div><!--/.navbar-collapse -->
     </div>
 </nav>
