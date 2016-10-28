ab3767d938c768a71d1dba215d5491031773396c
bannid <bannid@gmail.com>
2016-10-28 16:22:59 +1000

    Javascript Refactored

--- javascriptFunctions.js
+++ javascriptFunctions.js
@@ -1,5 +1,5 @@
 /*This file is part of On the Spot delivery website
-for Assignment of IFB299, SEM-2, 2016, QUT. This website 
+for Assignment of IFB299, SEM-2, 2016, QUT. This file 
 contains javascript functions.
 @author:Navjot Singh Dhaliwal
 */
@@ -16,7 +16,18 @@ function submitForm(formId, item, columm){
 	document.getElementById(columm+formId).value = ""+item;
 	document.getElementById("form"+formId).submit();
 }
-
+/*This function is specfically designed for order.php
+@author: Navjot singh Dhaliwal
+*/
+function validateOrderForm(formId){
+	validateAddresses('destination');
+	validateAddresses('pickup');
+	var noError = validateName("receiversname") && validateNumber("receiverscontact") && validateWeight("weight") && optionValidator("size") && validateIfTrue('destinationValidator') && validateIfTrue('pickupValidator');
+	if(noError == false){
+		window.scroll(0,0);
+	}
+	return noError;
+}
 /*This function will assign a driver and mark a package as
 no longer pending. Setting a timestamp for when this occurs
 as well
@@ -35,14 +46,36 @@ function assignDriver(formId, item, orderNo){
         data: { status: "Ready For Pickup", orderNo: orderNo}
     }); 
 }
-
-/*This function is specfically designed for order.php
-@author: Navjot singh Dhaliwal
-*/
-function submitForm2(formId){
-	var noError = validateName("receiversname") && validateNumber("receiverscontact") && validateWeight("weight") && optionValidator("size") && optionValidator("valuable");
-	return noError;
+function validateRegisterForm(){
+	validateAddresses("address");
+	var noError = validateEmail("email") && validatePassword("password") && validateNumber('contact') && validateIfTrue('address') && validateName("firstname") && validateName("lastname");
+	if(noError == false){
+		window.scroll(0,0);
+	}
+	return noError;	
 }
+
+/*This function will validate the address 
+@author:Navjot Singh Dhaliwal
+*/
+function validateAddresses(id){
+	var address = document.getElementById(id).value;
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address': address},function(results, status){
+	    if (status === google.maps.GeocoderStatus.OK && results.length == 1) {
+	    	valueChanger(id+"Validator",true);
+	    }
+	    else{
+	    	valueChanger(id+"Validator",false);
+	    }
+	});
+}
+
+function valueChanger(id,value){
+	var element = document.getElementById(id);
+	element.value = value;
+}
+
 /*This function will update status with timestamps
 Built from submitForm function
 @param:formId-> ID of form to submit.
@@ -75,12 +108,10 @@ function changeCursorType(cursorType,elementTag){
 /*Simple function to hide elements
 @author:Navjot Singh Dhaliwal
 */
-function hideElements(eventExecutor, button){
+function hideElements(eventExecutor){
 	var completed = 1;
 	var pending = 2;
 	var elements = document.getElementsByTagName('div');
-	$(button).removeClass("disabled").siblings().removeClass("active");
-	$(button).addClass("active").siblings().addClass("disabled");	
 	if(eventExecutor == completed){
 		for(var i = 0; i < elements.length;i++){
 			if(elements[i].getAttribute('name') == "Pending" ||	elements[i].getAttribute('name') == "Ready For Pickup" || elements[i].getAttribute('name') == "With Driver For Delivery" || elements[i].getAttribute('name') == "On Way to Warehouse" ||	elements[i].getAttribute('name') == "At Warehouse"){
@@ -112,17 +143,20 @@ function clickElement(ID){
 @author: Navjot Singh Dhaliwal
 */
 function validate(id,type,flag){
-	var valid;
+	if(flag !=false){
+		validateAddresses('destination');
+		validateAddresses('pickup');
+	}
+	else{
+		validateAddresses('address');
+	}
 	switch(type){
 		case "name":{
-			setTimeout(function(valid){validateName(id);},1);
+			setTimeout(function(){validateName(id);},1);
 			break;
 		}
 		case "password":{
 			setTimeout(function(){validatePassword(id);},1);
-		}
-		case "address":{
-			valid = validateAddress(id);
 			break;
 		}
 		case "number":{
@@ -131,7 +165,27 @@ function validate(id,type,flag){
 		}
 		case "weight":{
 			setTimeout(function(){validateWeight(id);},1);
+			break;
 		}
+		case "email":{
+			setTimeout(function(){validateEmail(id);},1);
+			break;
+		}
+	}
+}
+/*Helper function to validate email
+@author: Navjot Singh Dhaliwal
+*/
+function validateEmail(id) {
+	var email = document.getElementById(id).value;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(email)){
+		changeProperty(id,"color",true);
+		return true;
+	}
+	else{
+		changeProperty(id,"color",false);
+		return false;
 	}
 }
 /*Helper function for validate method. This function will help
@@ -140,7 +194,7 @@ to validate name
 */
 function validateName(id){
 	var string = document.getElementById(id).value;
-	if(/^[a-z A-Z]+$/.test(string)){
+	if(/^[a-z A-Z]+$/.test(string) && string.length < 20){
 		changeProperty(id,"color",true);
 		return true;
 	}
@@ -155,7 +209,7 @@ to validate password.
 */
 function validatePassword(id){
 	var string = document.getElementById(id).value;
-	if(/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/.test(string)){
+	if(/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/.test(string) && string.length > 8){
 		changeProperty(id,"color",true);
 		return true;
 	}
@@ -181,11 +235,16 @@ function validateNumber(id){
 	}
 }
 /*Helper function for validate method. This function will help
-to validate address.
+to validate destination.
 @author: Navjot Singh Dhaliwal
 */
-function validateAddress(id){
-
+function validateIfTrue(id){
+	var desElement = document.getElementById(id);
+	var destination = desElement.value;
+	if(destination == "false"){
+		return false;
+	}
+	return true;
 }
 /*Helper function for validate method. This function will help
 to validate weight.
@@ -200,7 +259,7 @@ function validateWeight(id){
 		return false;
 	}
 	weight = parseInt(string);
-	if(weight > maxWeight){
+	if(weight > maxWeight || weight <= 0){
 		changeProperty(id,"color",false);
 		return false;
 	}
@@ -225,7 +284,7 @@ function changeProperty(id,attribute,status){
 		displayOfElement = "block";
 	}
 	else{
-		color="red";
+		color="orangered";
 		displayOfElement = "none";
 	}
 	var element = document.getElementById(id);
@@ -240,22 +299,37 @@ function changeProperty(id,attribute,status){
 		}
 		case "display":{
 			element.style.display = displayOfElement;
+			break;
+		}
+		case "value":{
+			element.value = status;
+			break;
 		}
 	}
 }
 /*This function will append child into given element
 @author: Navjot Singh Dhaliwal
+@id->parent elment
 */
-function appendElementB(id,elementType,elementValue,idOfNewElement){
-	var element = document.createElement(elementType);
-	element.id = idOfNewElement;
-	element.style.cursor = "pointer";
-	var value = document.createTextNode(elementValue);
+function appendElement(id,elementType,elementValue,idOfNewElement){
 	var parent = document.getElementById(id);
-	element.appendChild(value);
-	parent.appendChild(element);
+	var newElement = document.createElement(elementType);
+	newElement.id = idOfNewElement;
+	newElement.value = elementValue;
+	parent.appendChild(newElement);
 }
-
+/**
+This function will create two elements which will be used to 
+validate destination and pick up address
+@author: Navjot Singh Dhaliwal
+*/
+function prepareOrder(){
+	appendElement("addressDiv","INPUT",false,"destinationValidator");
+	appendElement("addressDiv","INPUT",false,"pickupValidator");	
+}
+function prepareRegister(){
+	appendElement("addressDiv","INPUT",false,"addressValidator");
+}
 /*This function will check if address is correct and 
 append the formatted address to the given element.
 @copyright: google
@@ -264,23 +338,23 @@ append the formatted address to the given element.
 function doGeocode(id){
   	var addr = document.getElementById(id);
 	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({'address': addr.value, 
-						'region' : 'aus', 
-						'bounds': new google.maps.LatLngBounds(new google.maps.LatLng( -27.657579, 152.692471), new google.maps.LatLng( -27.184346, 153.277443)),
-						'componentRestrictions' : {country: 'AU'}}, function(results, status){
+	geocoder.geocode({'address': addr.value},function(results, status){
 	    if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
 	    	changeProperty(id,"color",true);
-	    	document.getElementById(id).value = results[0].formatted_address;
+	    	changeProperty(id,"value",results[0].formatted_address);
 	    }
 	    else{
 	    	changeProperty(id,"color",false);
 	    }
-	});
+	});	
 }
-
+/*Check if option has been selected
+@author: Navjot Singh Dhaliwal
+NOT GENERIC
+*/
 function optionValidator(id){
-	var string = document.getElementById(id).value;
-	if(string == ""){
+	var element = document.getElementById(id);
+	if(element.value == ""){
 		return false;
 	}
 	return true;
